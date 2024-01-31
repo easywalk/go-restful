e495504119869e59e8723986777532f3fb084949
Potato <dongju.paek@navercorp.com>
2024-01-31 19:55:49 +0900

    refactoring

--- repository.go
+++ repository.go
@@ -1,7 +1,7 @@
 package go_easywalk
 
 import (
-	"github.com/easywalk/go/restful/easywalk/repository"
+	"github.com/easywalk/go-restful/easywalk/repository"
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
